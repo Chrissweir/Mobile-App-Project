cb44de0f66650b2ab38fafc1b9137c96dcf9a6bd
Chrissweir <christopher.weir@hotmail.com>
2016-03-31 13:18:04 +0000

    Changes

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Settings : MonoBehaviour {
+
+    private float volume = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void options()
+    {
+        volume = GUILayout.HorizontalSlider(volume, 0.0f, 1.0f);
+        AudioListener.volume = volume;
+    }
+}
